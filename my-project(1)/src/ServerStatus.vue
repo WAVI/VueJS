31908b605baddc155dd77c97ef07dd6c7ba2b63e
Iustin Hilca <iustin_hilca@yahoo.com>
2017-09-15 10:56:54 +0200

    new changes, beginning of vue course

---
+++ my-project(1)/src/ServerStatus.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+    <p>Server status: {{ status }}</p>
+    <p>Server status: {{ iustin }}</p>
+    <hr>
+    <button @click="changeStatus">Change status</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => {
+            return {
+                status: 'Critical',
+                iustin: 's'
+            }
+        },
+        methods:{
+            changeStatus() {
+                this.status = 'Normal'
+            }
+        }
+    }
+</script>
