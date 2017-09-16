916fe551c85e9bbf0ea97bb6184b45dcbc0549a8
unknown <iustin_hilca@yahoo.com>
2017-09-16 17:15:13 +0200

    section 13

---
+++ sect 13/section13/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>Filters & Mixins</h1>
+    <p> {{ text | toUpperCase | to-lowercase }} </p>
+    <hr>
+    <button @click="fruits.push('Berries')"> Add New Item </button>
+    <input v-model="filterText">
+    <ul>
+     <li v-for="fruit in filteredFruits">{{fruit}}</li>
+    </ul>
+    <hr>
+    <app-list></app-list>
+  </div>
+</template>
+
+<script>
+
+  import List from './List.vue';
+  import { fruitMixin } from './fruitMixin'
+    //a filter is a syntax / feature u can use to transform some output in the template
+    export default {
+      mixins: [fruitMixin],
+      data() {
+        return  {
+          text: 'Hello there',
+        }
+      },
+      filters: {
+        toUpperCase(value) {
+          return value.toUpperCase();
+        }
+      },
+      components:{
+        appList: List
+      }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ sect 13/section13/src/List.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Filters & Mixins</h1>
+                <input v-model="filterText">
+                <ul>
+                  <li v-for="fruit in filteredFruits">{{fruit}}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    import { fruitMixin } from './fruitMixin';
+    //a filter is a syntax / feature u can use to transform some output in the template
+    export default {
+        mixins: [fruitMixin],
+        created(){
+            console.log('inside list created hook');
+        }
+    }
+</script>
+
+<style>
+
+</style>
